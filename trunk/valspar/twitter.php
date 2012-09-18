c1b0bb94ebddccbfdb80b564eb062dc8bf20b4c3
Lance@thetigerparty.com <Lance@thetigerparty.com@d865abdb-4238-0762-9bd5-0b9876a5f86d>
2012-09-18 16:12:29 +0000

    #2815 Testing

--- trunk/valspar/twitter.php
+++ trunk/valspar/twitter.php
@@ -1,19 +1,51 @@
 <?php
 
+session_start();
+
 require './twitter/tmhOAuth.php';
 require './twitter/tmhUtilities.php';
 
-$tmhOAuth = new tmhOAuth(array(
-  'consumer_key'    => 'rf0ohTLUkQ5qUftVpa1jSg',
-  'consumer_secret' => 'Y6QBh5fZPPyBklQC7Xf6CJeUjP7oWhENlg6XxxkPMtI',
-  'user_token'      => '744201864-VwqcqD68yvVyuknYb9BlxLMCv4h87Q45LnJNLjIG',
-  'user_secret'     => '1vP22OM6Fbj5brrdKQ4olvLDezhZJyuc4HRIqAN8',
+if (empty($_SESSION['access_token']) || empty($_SESSION['access_token']['oauth_token']) || empty($_SESSION['access_token']['oauth_token_secret'])) 
+{
+//    header('Location: ./clearsessions.php');
+/* Build TwitterOAuth object with client credentials. */
+	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET);
+	
+	/* Get temporary credentials. */
+	$request_token = $connection->getRequestToken(OAUTH_CALLBACK);
+
+	/* Save temporary credentials to session. */
+	$_SESSION['oauth_token'] = $token = $request_token['oauth_token'];
+	$_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
+ 
+	/* If last connection failed don't display authorization link. */
+	switch ($connection->http_code) {
+	  case 200:
+	    /* Build authorize URL and redirect user to Twitter. */
+	    $url = $connection->getAuthorizeURL($token);
+	    header('Location: ' . $url); 
+	    break;
+	  default:
+	    /* Show notification if something went wrong. */
+	    echo 'Could not connect to Twitter. Refresh the page or try again later.';
+	}
+
+}
+else
+{
+	$tmhOAuth = new tmhOAuth(array(
+		'consumer_key'    => 'rf0ohTLUkQ5qUftVpa1jSg',
+		'consumer_secret' => 'Y6QBh5fZPPyBklQC7Xf6CJeUjP7oWhENlg6XxxkPMtI',
+		'user_token'      => $_SESSION['access_token']['oauth_token'],
+		'user_secret'     => $_SESSION['access_token']['oauth_token_secret'],
 ));
+	
+}
 
 // we're using a hardcoded image path here. You can easily replace this with an uploaded image-see images.php example)
 // 'image = "@{$_FILES['image']['tmp_name']};type={$_FILES['image']['type']};filename={$_FILES['image']['name']}",
  
-$image = "./img/sample001.jpg";
+$image = "c:\\wamp\\www\\Valspar\\img\\sample001.jpg";
 //$image = "http://www.bespoke-app.com/media_storage/flower.jpg";
 
 //$filepath = "./img/button001.jpg"; $filetype = "image/jpeg"; $fileName = "button0001.jpg";
@@ -36,7 +68,7 @@ $code = $tmhOAuth->request('POST', 'https://upload.twitter.com/1/statuses/update
     'status'   => "testing" // Don't give up..
   ),
   true, // use auth
-  false  // multipart
+  true  // multipart
 );
 
 
@@ -44,7 +76,7 @@ $code = $tmhOAuth->request('POST', 'https://upload.twitter.com/1/statuses/update
 //*/
 //foreach( explode( "\n",$tmhOAuth->response['info']['request_header']) as $str)
 //	var_dump (  explode( "," , $str));
-var_dump ( explode( ",",$tmhOAuth->response['info']['request_header']));
+//var_dump ( explode( ",",$tmhOAuth->response['info']['request_header']));
 var_dump ($tmhOAuth->config);
 
 if ($code == 200)
