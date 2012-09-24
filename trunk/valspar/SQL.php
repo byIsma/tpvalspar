fcac363bcb04f6777b66b9ec2d04c00c030f8cec
Lance@thetigerparty.com <Lance@thetigerparty.com@d865abdb-4238-0762-9bd5-0b9876a5f86d>
2012-09-24 09:19:40 +0000

    #2814 #2815 #2822 #2827 finish

---
+++ trunk/valspar/SQL.php
@@ -0,0 +1,15 @@
+<?php	
+include("mysql_connect.php");
+if(isset($_POST['Name']) && isset($_POST['Email']) && isset($_POST['PictureSelect']))
+	$strSqlCommand = 'INSERT INTO sharelist ( Name, Email, PictureSelect) VALUES ( "'.$_POST['Name'].'", "'.$_POST['Email'].'", '.$_POST['PictureSelect'].')';
+else
+	$strSqlCommand = 'INSERT INTO sharelist ( Name, Email, PictureSelect) VALUES ( "'.$_SESSION['Name'].'", "'.$_SESSION['Email'].'", '.$_SESSION['PictureSelect'].')';
+	
+if (!mysql_query($strSqlCommand))
+{
+	echo "<Error Message=\"".mysql_error()."\" Domain=\"CreateGroup\" />";
+	echo "<Error Message=\"".$strSqlCommand."\" Domain=\"CreateGroup\" />";
+	exit;
+}
+
+?>
