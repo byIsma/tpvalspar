e0cb7c6870a6e1ced5f9e068ce0e7c343ad6dabc
Lance@thetigerparty.com <Lance@thetigerparty.com@d865abdb-4238-0762-9bd5-0b9876a5f86d>
2012-09-27 13:14:52 +0000

    #2864  Add design Page (part of twitter)

--- trunk/valspar/twitter/index2.php
+++ trunk/valspar/twitter/index2.php
@@ -43,7 +43,7 @@ $connection = new tmhOAuth(array(
 
 /* statuses/update */
 //$image = "./button001.jpg";
-$image = "c:\\wamp\\www\\Valspar\\Assets\\01_Tab\\House_".$_GET['ID'].".png";
+$image = "c:\\wamp\\www\\Valspar\\img\\sample00".$_GET['ID'].".jpg";
 //$filepath = "button001.jpg"; $filetype = "image/jpeg"; $fileName = "button0001.jpg";
 //$image = "{$filepath};type={$filetype};filename={$fileName}";
 date_default_timezone_set('GMT');
@@ -51,36 +51,67 @@ date_default_timezone_set('GMT');
 //$parameters = array('status' => "Why I can't share", 'media[]' => "@{$image};type=image/jpeg;filename={$image}" );
 //$status = $connection->post('statuses/update_with_media', $parameters);
 
-//$code = $tmhOAuth->request('POST', 'https://api.twitter.com/1.1/statuses/update_with_media.json',
+//$code = $tmhOAuth->request('POST', 'https://api.twitter.com/1.1/statuses/update_with_media.json',https://valspar.thetigerparty.com/Valspar/twitter/
 
+echo '
+<html>
+<head>
+<script src="../js/jquery-latest.js"></script>
+<script type="text/javascript">
+	$(document).ready(function(e)
+	{
+		$("#ImgBtTW").click(function(e)
+		{
+			var String = $("#MyString").text();
+				$.post( "index.php?ID='.$_GET['ID'].'&String="+String,
+				function(data)
+			{
+				data = data;
+			});
+		});
+	});
+</script>
+</head>
+<body>
+<div style="background-image:url(../Assets/Facebook_Twitter_Icons/Twitter_BG.png); width:546px; height:297px">
+	<div style="position: absolute; top: 110px; left: 24px; width: 501px; height: 53px;">
+		<textarea rows="4" cols="70" id="MyString"></textarea>
+	</div>
+	<div style="position: absolute; top: 185px; left: 29px;">
+		<img src="../Assets/Facebook_Twitter_Icons/Color_'.$_GET['ID'].'_icon.png" width="50px"/>
+	</div>
+	<div style="position: absolute; top: 250px; left: 460px;">
+		<img id="ImgBtTW" src="../Assets/03_Post_Donate/Tweet_OnMouseOver.png"/>
+	</div>
+</div>
+</body>
+</html>
+';
+/*
 $code = $connection->request('POST', 'https://upload.twitter.com/1/statuses/update_with_media.json',
 	array(
-    	'media[]'  => "@{$image};type=image/png;filename={$image}",
-    	'status'   => $_GET['String']//"Valspar Love Your Color Guarabtee Project. https://apps.facebook.com/valspar/" // Don't give up..
+    	'media[]'  => "@{$image};type=image/jpeg;filename={$image}",
+    	'status'   => "Valspar Love Your Color Guarabtee Project. https://apps.facebook.com/valspar/" // Don't give up..
   	),
   	true, // use auth
   	true  // multipart
 );
 if ($code == 200)
 {
-	/* Create a TwitterOauth object with consumer/user tokens. */
+	// Create a TwitterOauth object with consumer/user tokens. 
 	$Getter = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
-	/* If method is set change API call made. Test is called by default. */
+	// If method is set change API call made. Test is called by default. 
 	$content = $Getter->get('account/verify_credentials');
 
 $_SESSION['Name'] = $content->screen_name;
 $_SESSION['Email'] = $content->id_str;
 $_SESSION['PictureSelect'] = $_GET['ID'];
 	require( "../SQL.php");
-//	var_dump($content->id_str);
-//	var_dump($content->name);
 
-//  tmhUtilities::pr(json_decode($connection->response['response']));
 	header( 'Location: '.json_decode($connection->response['response'])->entities->media[0]->expanded_url );
-//	var_dump( json_decode($connection->response['response'])->entities );
 }
 else
 {
   tmhUtilities::pr($connection->response['response']);
 }
-
+*/
