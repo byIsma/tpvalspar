d9af6e32b42d77e0973173ed84884cbfedfe5765
lance <Lance@thetigerparty.com>
2012-10-12 06:34:22 +0000

    #2902 #2877

--- valspar/index3.php
+++ valspar/index3.php
@@ -35,7 +35,7 @@ $attachment =  array(
 );
 
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL,'https://graph.facebook.com/me/og.likes');
+curl_setopt($ch, CURLOPT_URL,'https://graph.facebook.com/me/likes');
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
 curl_setopt($ch, CURLOPT_POST, true);
