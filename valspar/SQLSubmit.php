d604ce7c4953deedc79c656b9286f91e6ae718cc
wisely <wisely@thetigerparty.com>
2012-10-23 04:23:12 +0000

    Detect user is Facebook fan or not #2971

--- valspar/SQLSubmit.php
+++ valspar/SQLSubmit.php
@@ -1,10 +1,12 @@
 ﻿<?php	
 include("mysql_connect.php");
 session_start();
-if(isset($_POST['Name']) && isset($_POST['Email']) && isset($_POST['PictureSelect']))
+//if(isset($_POST['Name']) && isset($_POST['Email']) && isset($_POST['PictureSelect']))
+if(isset($_POST['PictureSelect']))
 {
-	$strSqlCommand = 'INSERT INTO SubmitList ( Name, Email, PictureSelect) VALUES ( "'.$_POST['Name'].'", "'.$_POST['Email'].'", '.$_POST['PictureSelect'].')';
-
+	//$strSqlCommand = 'INSERT INTO SubmitList ( Name, Email, PictureSelect) VALUES ( "'.$_POST['Name'].'", "'.$_POST['Email'].'", '.$_POST['PictureSelect'].')';
+	$strSqlCommand = 'INSERT INTO SubmitList (PictureSelect) VALUES ('.$_POST['PictureSelect'].')';
+	
 	if (!mysql_query($strSqlCommand))
 	{
 		echo "<Error Message=\"".mysql_error()."\" Domain=\"CreateGroup\" />";
