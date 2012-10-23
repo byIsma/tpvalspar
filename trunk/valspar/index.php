e1f619a5674383da6fb9b4b1783d0d85be199a0e
wisely@thetigerparty.com <wisely@thetigerparty.com@d865abdb-4238-0762-9bd5-0b9876a5f86d>
2012-10-23 04:23:12 +0000

    Detect user is Facebook fan or not #2971

--- trunk/valspar/index.php
+++ trunk/valspar/index.php
@@ -8,7 +8,7 @@ $headers = "From:" . $from;
 mail($to,$subject,$message,$headers);
 echo "Mail Sent.";
 */
-header( 'Location: index.html' ) ;
+//header( 'Location: index.html' ) ;
 
 require './facebook/src/facebook.php';
 
@@ -16,50 +16,25 @@ require './facebook/src/facebook.php';
 $facebook = new Facebook(array(
   'appId'  => '492668300762232',
   'secret' => 'b637c96d95926c23f6d38835296951ac',
+  'cookie' => true
 ));
 
-// Get User ID
-$user = $facebook->getUser();
-function ShareImage()
-{			
-if($user)
-{
-      // We have a user ID, so probably a logged in user.
-      // If not, we'll get an exception, which we handle below.
-      try {
-        $ret_obj = $facebook->api('/me/feed', 'POST',
-                                    array(
-                                      'link' => 'apps.facebook.com/valspar/',
-									  'source' => 'http://valspar.thetigerparty.com/Valspar/img/sample001.jpg' ,
-                                      'message' => 'Share a Image from Valspar',
-									  'description' => 'Share a Image from Valspar'
-                                 ));
-//        echo '<pre>Post ID: ' . $ret_obj['id'] . '</pre>';
+$signed_request = $facebook->getSignedRequest();
 
-      } catch(FacebookApiException $e) {
-        // If the user is logged out, you can have a 
-        // user ID even though the access token is invalid.
-        // In this case, we'll get an exception, so we'll
-        // just ask the user to login again here.
-        $login_url = $facebook->getLoginUrl( array(
-                       'scope' => 'publish_stream'
-                       )); 
-        echo 'Please <a href="' . $login_url . '">login.</a>';
-        error_log($e->getType());
-        error_log($e->getMessage());
-      }   
-    } 
-	else
-	{
-
-      // No user, so print a link for the user to login
-      // To post to a user's wall, we need publish_stream permission
-      // We'll use the current URL as the redirect_uri, so we don't
-      // need to specify it here.
-      $login_url = $facebook->getLoginUrl( array( 'scope' => 'publish_stream' ) );
-      echo 'Please <a href="' . $login_url . '">login.</a>';
-
-    } 						
+$page_id = $signed_request["page"]["id"];
+$page_admin = $signed_request["page"]["admin"];
+$like_status = $signed_request["page"]["liked"];
+$country = $signed_request["user"]["country"];
+$locale = $signed_request["user"]["locale"];
+ 
+if ($like_status) {
+	//$a = file_get_contents("submit.html");
+	//echo ($a);
+	header( 'Location: submit.html' ) ;
+} else {
+	// If a non-fan is on your page
+	//$a = file_get_contents("submit.html");
+	//echo ($a);
 }
 
 ?>
@@ -68,22 +43,11 @@ if($user)
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-
+	<link rel="stylesheet" type="text/css" href="CSS/index.css" />
         <script src="js/jquery-latest.js"></script>
+        <script src="js/jquery.lightbox_me.js"></script>
         <script type="text/javascript">
-			window.fbAsyncInit = function()
-			{
-				FB.init(
-				{
-					appId      : '492668300762232', // App ID
-					//channelUrl : '//apps.facebook.com/channel.html', // Channel File
-					status     : true, // check login status
-					cookie     : true, // enable cookies to allow the server to access the session
-					xfbml      : true  // parse XFBML
-				});
-				//FB.Canvas.setAutoResize();
-				FB.Canvas.setAutoGrow();
-			};
+	
 			// Load the SDK Asynchronously
 			(function(d)
 			{
@@ -94,34 +58,31 @@ if($user)
 				ref.parentNode.insertBefore(js, ref);
 			}(document));
 
-			if( location.search.indexOf("GetDC") == -1 )
+			window.fbAsyncInit = function()
 			{
-				var oauth_url = 'https://www.facebook.com/dialog/oauth/';
-				oauth_url += '?client_id=492668300762232';
-				oauth_url += '&redirect_uri=' + encodeURIComponent('https://valspar.thetigerparty.com/Valspar/');
-				oauth_url += '&scope=publish_stream';
-				oauth_url += '&state=GetDC';
-				window.top.location = oauth_url;
-			}
-			else if( location.search.indexOf("AfterGetDC") == -1 )
-			{
-				var oauth_url = 'https://www.facebook.com/dialog/oauth/';
-				oauth_url += '?client_id=492668300762232';
-				oauth_url += '&redirect_uri=' + encodeURIComponent('https://apps.facebook.com/valspar/');
-				oauth_url += '&scope=publish_stream';
-				oauth_url += '&state=AfterGetDC';
-				window.top.location = oauth_url;
-			}
-				var ID = 1;
-
+				FB.init(
+				{
+					appId      : '492668300762232', // App ID
+					//channelUrl : '//apps.facebook.com/channel.html', // Channel File
+					status     : true, // check login status
+					cookie     : true, // enable cookies to allow the server to access the session
+					xfbml      : true  // parse XFBML
+				}); 
+			 
+			}; 
+			
+			
+			
+				var ID = 0;
+				var SubmitID = 0;
+				var UserId = 0;
 			$(document).ready(function(e)
 			{
-				$("a[id^='Img']").click(function(e)
-				{
-					ID = this.id.substring(4);
-					$('.item').attr("src","http://valspar.thetigerparty.com/Valspar/img/sample00"+ID+".jpg");
-				});
+				var colorname;
 
+				//grow();
+				 
+				
 				$("#EMShare").click(function(e)
 				{
 //					var RTFBody = $('.item').attr("src");
@@ -135,115 +96,196 @@ if($user)
 
 				});
 
-				$("#FBShare").click(function(e)
+				$("#ImgBtFB").click(function(e)
 				{
 					var result = FB.ui(
 					{
 						method: 'feed',
-						name: 'Valspar Love Your Color Guarantee Project',
 						link: 'https://apps.facebook.com/valspar',
-						picture: $('.item').attr("src"),
-						caption: 'As a part of the Valspar Love Your Color Guarabtee Project, I helped guarabtee a donation to Habitat for Humanity. You Can, too.'//,
-//						properties: {'You Can, too.':'https://apps.facebook.com/valspar'}
+						picture: 'http://valspar.thetigerparty.com/Valspar/Assets/Facebook_Twitter_Icons/Color_'+ID+'_icon.png',
+						name: 'Valspar Love Your Color Guarantee Project',
+						caption: 'As a part of the Valspar Love Your Color Guarantee Project, I helped guarantee a donation to Habitat for Humanity by painting this house Valspar '+ colorname +'.',
+						description: 'You can help, too!'
 					},
 					function(response)
 					{
 						if (response && response.post_id)
 						{
-//							alert('Post was published.');
-							FB.api('/me', function(response)
+							$('#PopupCanvas').trigger('close');
+							//window.location.href = "thankyou.php";	
+							$.post( 'https://valspar.thetigerparty.com/Valspar/SQL.php',	{"SubmitID": SubmitID}, function(data)
 							{
-								$.post( 'https://valspar.thetigerparty.com/Valspar/SQL.php',	{"Name": response.name, "Email": response.email, "PictureSelect": ID}, function(data)
-								{
-								});
 							});
 						}
 						else
 						{
-//							alert('Post was not published.');
-//alert( strUserName );
-//alert( strUserEmail );
-//							FB.getLoginStatus(function(response)
-//							{
-//								 response;
-//							});
 						}
 					});							
-//*/
 
 				});
-				$("#TWShare").click(function(e)
+				$("#ImgBtTW").click(function(e)
 				{
-					window.open( "https://valspar.thetigerparty.com/Valspar/twitter/index.php?ID="+ID, "Share Twitter",'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=800,height=300');
-/*					var xmlhttp;
-					if (window.XMLHttpRequest)
-					{// code for IE7+, Firefox, Chrome, Opera, Safari
-						xmlhttp=new XMLHttpRequest();
-					}
-					else
-					{// code for IE6, IE5
-						xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-					}
-					alert(ID);
-					var params = "ID=" + ID;
-					
-					xmlhttp.onreadystatechange=function()
-					{
-						if (xmlhttp.readyState==4 && xmlhttp.status==200)
-						{
-							if( xmlhttp.responseText == "200" )
-								alert( "Share Image To Twitter Success!!" );
-							else
-								alert( xmlhttp.responseText );
-							
-						}
-					}
-					xmlhttp.open("POST","https://valspar.thetigerparty.com/Valspar/twitter/index.php",true);
-					
-					xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-					xmlhttp.setRequestHeader("Content-length", params.length);
-					xmlhttp.setRequestHeader("Connection", "close");
-
-					xmlhttp.send( params );	
-*/				
+					window.open( "https://valspar.thetigerparty.com/Valspar/twitter/index2.php?ID="+ID+"&SubmitID="+SubmitID, "Share Twitter",'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=550,height=300');
+					 
+					$('#PopupCanvas').trigger('close');	
+					//window.location.href = "thankyou.php";	
 				});
+		
+				
+				$("img[id^='ImgBt0']").mouseout(function(e)
+				{
+					$("#"+this.id).attr("src","Assets/01_Tab/Color_"+ this.id.substring(6) +".png");
+				});
+				$("img[id^='ImgBt0']").mouseover(function(e){
+					$("#"+this.id).attr("src","Assets/01_Tab/Color_"+ this.id.substring(6) +"_OnMouseOver.png");
+				});
+				$("img[id^='ImgBt0']").click(function(e)
+				{
+					ID = this.id.substring(6);
+					$("#ImgHouse").attr("src","Assets/01_Tab/House_"+ID+".png");
+					$("#ImgBtSu").attr("src","Assets/01_Tab/Submit_Btn.png");
+					$("#ImgBtSu").css("cursor","pointer");
+					colorname = $(this).attr("data");
+				});
+
+				//$("#FBLike").mouseout(function(e){ $("#FBLike").attr("src","Assets/00_Fangate/LikeBtn.png"); });
+				$("#loveyourcolor").mouseout(function(e){ $("#loveyourcolor").attr("src","Assets/00_Fangate/loveyourcolor_link.png"); });
+				$("#ImgBtTx").mouseout(function(e){ $("#ImgBtTx").attr("src","Assets/01_Tab/LoveYourColor.png"); });
+				$("#ImgBtFB").mouseout(function(e){ $("#ImgBtFB").attr("src","Assets/03_Post_Donate/Facebook_Share.png"); });
+				$("#ImgBtTW").mouseout(function(e){ $("#ImgBtTW").attr("src","Assets/03_Post_Donate/Tweet.png"); });
+				
+				$("#FBLike").mouseover(function(e){ $("#FBLike").attr("src","Assets/00_Fangate/LikeBtn_OnMouseOver.png"); });
+				$("#loveyourcolor").mouseover(function(e){ $("#loveyourcolor").attr("src","Assets/00_Fangate/loveyourcolor_link_OnMouseOver.png"); });
+				$("#ImgBtTx").mouseover(function(e){ $("#ImgBtTx").attr("src","Assets/01_Tab/LoveYourColor_OnMouseOver.png"); });
+				$("#ImgBtFB").mouseover(function(e){ $("#ImgBtFB").attr("src","Assets/03_Post_Donate/Facebook_Share_OnMouseOver.png"); });
+				$("#ImgBtTW").mouseover(function(e){ $("#ImgBtTW").attr("src","Assets/03_Post_Donate/Tweet_OnMouseOver.png"); });
+
+				$("#ImgBtSu").mouseup(function(e)
+				{
+					if(ID > 0)
+						$("#ImgBtSu").attr("src","Assets/01_Tab/Submit_Btn.png");
+				});
+				$("#ImgBtSu").mousedown(function(e)
+				{
+					if(ID > 0)
+						$("#ImgBtSu").attr("src","Assets/01_Tab/Submit_Btn_OnClick.png");
+				});
+				$("#ImgBtSu").click(function(e)
+				{ 
+					if(ID > 0)
+					{
+								FB.api('/me', function(response)
+								{
+									//$.post( 'https://valspar.thetigerparty.com/Valspar/SQLSubmit.php',	{"Name": response.name, "Email": response.email, "PictureSelect": ID}, function(data)
+									//{
+									//	SubmitID = data.split("\n")[2];
+										 
+									//	$('#PopupCanvas').lightbox_me({
+									//		centered: true,
+									//		onClose: function(){
+									//			//window.location.href = "thankyou.php";
+									//		}
+									//	}); 
+									//});
+								 
+									$.post( 'https://valspar.thetigerparty.com/Valspar/SQLSubmit.php',	{"PictureSelect": ID}, function(data)
+									{
+										SubmitID = data.split("\n")[2];
+										 
+										$('#PopupCanvas').lightbox_me({
+											centered: true,
+											onClose: function(){
+												//window.location.href = "thankyou.php";
+											}
+										}); 
+									});
+								});
+						
+						
+					}
+				});
+				
+				$("#ImgBtClose").mouseup(function(e){ $("#ImgBtClose").attr("src","Assets/03_Post_Donate/Close.png"); });
+				$("#ImgBtClose").mousedown(function(e){ $("#ImgBtClose").attr("src","Assets/03_Post_Donate/Close_OnClick.png"); });
+				$("#ImgBtClose").click(function(e){ 
+					$('#PopupCanvas').trigger('close'); 
+					
+				});
+				
+				 
 			});
 			
+			function GotoSubmitPage()// onclick="GotoSubmitPage()"
+			{
+				$("#IndexPage").hide();
+				$("#SubmitPage").show();
+			}
+
+			function grow()
+			{
+			    FB.Canvas.setAutoGrow(); 
+			    
+			    FB.getLoginStatus(function(response) {
+			        var status = response.status;
+					if (status == 'connected') { 
+					} else if(status == 'notConnected') {
+					 
+					} else {
+				              
+			        }
+				});
+			}
+			
+			var iframe = $('.fb-like')[0].childNodes[0].childNodes[0];
+			alert( "Frame title: " + iframe.contentWindow.title );
+  
         </script>
 <title>Valspar</title>
 </head>
 
-<body style="overflow:hidden">
+<body style="width:100%;height:1160px;overflow:hidden;margin:0; padding:0; border:0;" onLoad="grow();">
 
-<div class="SliderImage" align="center">
-	<div class="slider" style="width:800px; height:600px; overflow:hidden">
-		<img class="item" src="http://valspar.thetigerparty.com/Valspar/img/sample001.jpg" id="Img001"/>
-		<!--img class="item" src="img/sample002.jpg" id="Img002"/>
-		<img class="item" src="img/sample003.jpg" id="Img003"/>
-		<img class="item" src="img/sample004.jpg" id="Img004"/>
-		<img class="item" src="img/sample005.jpg" id="Img005"/-->
-	</div>
-    <div style="width:800px">
-    	<div style="float:left;"><a type="button" id="Img01"><img src="img/button001.jpg"/></a></div>
-    	<div style="float:left;"><a type="button" id="Img02"><img src="img/button002.jpg"/></a></div>
-    	<div style="float:left;"><a type="button" id="Img03"><img src="img/button003.jpg"/></a></div>
-    	<div style="float:left;"><a type="button" id="Img04"><img src="img/button004.jpg"/></a></div>
-    	<div style="float:left;"><a type="button" id="Img05"><img src="img/button005.jpg"/></a></div>
+<div id="IndexPage" style="height:100%">
+	<div id="FongateCanvas">
+    	<img src="Assets/00_Fangate/00Fangate_BG.png"  /><!--style="cursor:pointer;"-->
+		<span class="objinside" id="ImgLink">
+        	<a href="http://www.loveyourcolor.com/">
+            	<img src="Assets/00_Fangate/loveyourcolor_link.png" id="loveyourcolor" style="border:0 none;"/>
+            </a>
+        </span>
+        <div class="objinside" id="ImgLike">
+        	<img src="Assets/00_Fangate/LikeBtn.png" id="FBLike" style="border:0 none;"/>
+        	<!--a href="http://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fapps.facebook.com%2Fvalspar&send=false&layout=button_count&width=113&show_faces=false&action=like&colorscheme=light&font=arial&height=21&appId=492668300762232">
+        		
+            </a-->
+       	  <!--div class="fb-like" data-href="https://apps.facebook.com/valspar" data-send="false" data-layout="button_count" data-width="113" data-show-faces="false" data-font="arial"></div-->
+    	</div>
+		<div class="objinside" id="ImgLink"></div>
     </div>
-    
-    <div style="width:800px">
-    	<div style="float:left;padding-left:14%;">
-        	<a type="button" id="EMShare"><img src="img/mail_share.jpg" style="height:48px;"/></a>
-        	<!--input type="button" id="FBShare" value="Share To Facebook"/-->
-        </div>
-    	<div style="float:left;padding-left:14%;">
-        	<a type="button" id="FBShare"><img src="img/facebook-sharing.jpg" style="height:48px;"/></a>
-        	<!--input type="button" id="FBShare" value="Share To Facebook"/-->
-        </div>
-    	<div style="float:left;padding-left:14%;">
-        	<a type="button" id="TWShare"><img src="img/twitter_share.png" style="height:48px;"/></a>
-			<!--input type="button" id="TWShare" value="Share To Twitter"/-->
+</div>
+ 
+<div id="SubmitPage">
+	<div id="MainCanvas">
+		<div class="objinside" id="DivImgHouse"><img src="" id="ImgHouse" /></div>
+
+		<div class="objinside" id="DivImgColor1"><img src="Assets/01_Tab/Color_1.png" id="ImgBt01" data="COOL RAIN"/></div>
+		<div class="objinside" id="DivImgColor2"><img src="Assets/01_Tab/Color_2.png" id="ImgBt02" data="MARTIAN"/></div>
+		<div class="objinside" id="DivImgColor3"><img src="Assets/01_Tab/Color_3.png" id="ImgBt03" data="POOL PARTY"/></div>
+		<div class="objinside" id="DivImgColor4"><img src="Assets/01_Tab/Color_4.png" id="ImgBt04" data="SONIC PLUM"/></div>
+		<div class="objinside" id="DivImgColor5"><img src="Assets/01_Tab/Color_5.png" id="ImgBt05" data="RASPBERRY SORBET"/></div>
+        
+		<div class="objinside" id="DivImgBtSubmit"><img src="Assets/01_Tab/Submit_Btn_Disabled.png" id="ImgBtSu" /></div>
+		<div class="objinside" id="DivImgBtText">
+			<a target="_blank" href="http://www.loveyourcolor.com/">
+				<img src="Assets/01_Tab/LoveYourColor.png" id="ImgBtTx" style="border:0 none;" />
+			</a>
 		</div>
+	</div>
+    <div class="objinside" id="PopupCanvas">
+		<!--div style="position:absolute; left:64px; top:472px"><img src="Assets/03_Post_Donate/Post_Donate_BG.png" id="FBLike" /></div-->
+		<div class="objinside" id="DivImgBtFB"><img src="Assets/03_Post_Donate/Facebook_Share.png" id="ImgBtFB" /></div>
+		<div class="objinside" id="DivImgBtTW"><img src="Assets/03_Post_Donate/Tweet.png" id="ImgBtTW" /></div>
+		<div class="objinside" id="DivImgBtClose"><img src="Assets/03_Post_Donate/Close.png" id="ImgBtClose" /></div>
     </div>
 </div>
 
